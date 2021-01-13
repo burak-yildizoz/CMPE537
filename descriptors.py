4b4b1b6fa519e7f0a3fd7cf611216bc74ea73b83
Oguzhan-Sevim <61991731+Oguzhan-Sevim@users.noreply.github.com>
2021-01-14 01:30:00 +0300

    Update descriptors.py
    HOG descriptor is added.

--- descriptors.py
+++ descriptors.py
@@ -1,5 +1,55 @@
 import cv2 as cv
 
+class HOG:
+    def detectAndCompute(self, image, dummy=1):
+        ## This function takes an RGB image as input. First, the image is turned into Gray scale. Then, HOG is calculated.
+        resize=(128,128)
+        grid_size=(8,8)
+        num_of_bins=60
+        
+        grid_dim = int(resize[0]/grid_size[0])
+        
+        # Resize the image to a fixed size:
+        image = cv.resize(image,resize)
+        
+        # Convert the BGR to grayscale format:
+        image = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+        
+        # Create an empty array for the descriptor with proper dimensions:
+        hist = np.zeros((grid_size[0]*grid_size[1] , num_of_bins)) 
+        
+        # Calculate the gradients in x and y axis:
+        Gradx = cv.Sobel(image,cv.CV_32F,1,0,ksize=3) # By using cv2.CV_32F, gradient of each pixel will be 32-bit floating numbers
+        Grady = cv.Sobel(image,cv.CV_32F,0,1,ksize=3)
+        
+        # Find the angles between the gradients in radians:
+        GradRadian = np.arctan2(Grady,Gradx) # Each element is between -3.14 and 3.14
+        
+        # Create an array that contains centers of grids (dummy keypoints):
+        dummy_kps = np.zeros((grid_size[0]*grid_size[1],2))
+        
+        # Scan the image by the windows and find the histogram of each window. Then, concatenate them column wise:
+        for i in range(grid_size[0]):
+            for j in range(grid_size[1]):
+                
+                dummy_kps[i*grid_size[0]+j,0] = i*grid_dim + grid_dim/2
+                dummy_kps[i*grid_size[0]+j,1] = j*grid_dim + grid_dim/2
+    
+                # Create a mask that has the same size with images. The mask will be dark (0) in all areas except the
+                # desired window location. Window will be white (255).
+                mask = np.zeros(resize, np.uint8)
+                mask[int(i*(resize[0]/grid_size[0])) : int((i+1)*(resize[0]/grid_size[0])), int(j*(resize[1]/grid_size[1])) : int((j+1)*(resize[1]/grid_size[1]))] = 255
+                        
+                # Find the L1 norm histograms of each window:
+                hist_window = cv.calcHist([GradRadian],[0],mask,[num_of_bins],[-np.pi,np.pi])
+                hist_window = hist_window / np.sum(np.abs(hist_window)) # Divide the histogram array by its L1 norm, so it adds up to 1.
+    
+                # Add the histogram of current cell to the overall descriptor matrix:
+                hist[i*grid_size[0]+j] = hist_window[:,0]
+    
+        return dummy_kps, hist
+
+
 def get_descriptor(descname):
     # https://docs.opencv.org/3.4/d0/d13/classcv_1_1Feature2D.html
     if descname == 'SIFT':
@@ -8,6 +58,8 @@ def get_descriptor(descname):
         return cv.xfeatures2d.SURF_create()
     elif descname == 'ORB':
         return cv.ORB_create()
+    elif descname == 'HOG':
+        return HOG()
     else:
         raise Exception('Invalid option')
 
