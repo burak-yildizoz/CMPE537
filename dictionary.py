80f8147541f0f0fb2a1da97f6e70e4451d85a001
burak-yildizoz <45322678+burak-yildizoz@users.noreply.github.com>
2021-01-13 20:39:10 +0300

    imgops add loop_images applies given function on each image add load_array, complement of save_array

--- dictionary.py
+++ dictionary.py
@@ -9,35 +9,16 @@ def get_dictionary(dictname, K=21):
     else:
         raise Exception('Invalid option')
 
-def add_descriptors(dictionary, descriptor,
-                    desc_per_img=20,
-                    dict_path='Dataset/Caltech20/training/',
-                    dry_run=False):
-    if not dry_run:
-        add_descriptors(dictionary, descriptor, desc_per_img, dict_path, True)
-    taxons = os.listdir(dict_path)
-    for i_t, taxon in enumerate(taxons, start=1):
-        taxon_path = os.path.join(dict_path, taxon)
-        imnames = os.listdir(taxon_path)
-        for i_i, imname in enumerate(imnames, start=1):
-            impath = os.path.join(taxon_path, imname)
-            assert os.path.isfile(impath), 'Could not find %s' % (impath)
-            if dry_run: continue
-            img = cv.imread(impath)
-            assert img is not None, 'Could not read %s %s' % (taxon, imname)
-            print('[%d/%d] [%d/%d] %s %s' %
-                  (i_t, len(taxons), i_i, len(imnames), taxon, imname))
-            _, desc = descriptor.detectAndCompute(img, None)
-            if desc is None: continue
-            np.random.shuffle(desc)
-            desc = desc[0:desc_per_img, :]
-            dictionary.add(desc)
-        if dry_run: print('%s has %d images' % (taxon, len(imnames)))
-    if dry_run: print('dry-run finished')
+def add_descriptors(img, dictionary, descriptor, desc_per_img=20):
+    _, desc = descriptor.detectAndCompute(img, None)
+    if desc is None: pass
+    np.random.shuffle(desc)
+    desc = desc[0:desc_per_img, :]
+    dictionary.add(desc)
 
 if __name__ == '__main__':
     from descriptors import get_descriptor
-    from imgops import save_array
+    import imgops
     from timeit import default_timer as timer
     # parameters
     dictname = 'BOW'
@@ -47,13 +28,14 @@ if __name__ == '__main__':
     dictionary = get_dictionary(dictname)
     descriptor = get_descriptor(descname)
     start = timer()
-    add_descriptors(dictionary, descriptor, desc_per_img)
+    imgops.loop_images(add_descriptors, (dictionary, descriptor, desc_per_img))
     end = timer()
     # print and save the results
     print('Collecting descriptors took %.1f seconds' % (end - start))
     print('Total number of descriptors:', dictionary.descriptorsCount())
     descs = np.vstack(dictionary.getDescriptors())
-    save_array('descs_%s_%s_%d' % (dictname, descname, desc_per_img), descs)
+    name = 'descs_%s_%s_%d' % (dictname, descname, desc_per_img)
+    imgops.save_array(name, descs)
     # obtain the vocabulary
     start = timer()
     vocab = dictionary.cluster(descs)
@@ -61,4 +43,5 @@ if __name__ == '__main__':
     # print and save the results
     print('Clustering took %.1f seconds' % (end - start))
     print('vocabulary:', vocab.shape)
-    save_array('vocab_%s_%s_%d' % (dictname, descname, desc_per_img), vocab)
+    name = 'vocab_%s_%s_%d' % (dictname, descname, desc_per_img)
+    imgops.save_array(name, vocab)
--- imgops.py
+++ imgops.py
@@ -13,9 +13,38 @@ def plt_imshow(img):
     plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
     plt.show()
 
+def loop_images(func,   # function to be called on each image
+                params, # func(img, *params)
+                taxons_path='Dataset/Caltech20/training/',
+                dry_run=True):
+    taxons = os.listdir(taxons_path)
+    for i_t, taxon in enumerate(taxons, start=1):
+        taxon_path = os.path.join(taxons_path, taxon)
+        imnames = os.listdir(taxon_path)
+        for i_i, imname in enumerate(imnames, start=1):
+            impath = os.path.join(taxon_path, imname)
+            assert os.path.isfile(impath), 'Could not find %s' % (impath)
+            if dry_run: continue
+            img = cv.imread(impath)
+            assert img is not None, 'Could not read %s %s' % (taxon, imname)
+            print('[%d/%d] [%d/%d] %s %s' %
+                  (i_t, len(taxons), i_i, len(imnames), taxon, imname))
+            func(img, *params)
+        if dry_run: print('%s has %d images' % (taxon, len(imnames)))
+    if dry_run:
+        print('dry-run finished')
+        loop_images(func, params, taxons_path, dry_run=False)
+
 NPYDIR = 'Data/'
+
 def save_array(name, array):
     os.makedirs(NPYDIR, exist_ok=True)
     path = NPYDIR + name + '.npy'
     np.save(path, array)
     print('Saved to', path)
+
+def load_array(arrtype, dictname, descname, desc_per_img):
+    assert arrtype in ['descs', 'vocab']
+    name = '%s_%s_%s_%d' % (arrtype, dictname, descname, desc_per_img)
+    arr = np.load(NPYDIR + name + '.npy')
+    return arr
